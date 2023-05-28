12a4495a8cece831cd73a83c426a6a5276908822
keith li <keith.li@waiongroup.com>
2023-05-29 01:32:44 +0800

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Note from './components/Note'
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.lenght + 1
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+
+  }
+  const handlenotechange = (event) => {
+    event.preventDefault()
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <ul>
+          {notes.map(note =>
+            <Note key={note.id} note={note} />
+          )}
+        </ul>
+        <form onSubmit={addNote}>
+          <button type='submit'>save</button>
+        </form>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
